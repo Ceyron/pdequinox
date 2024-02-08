9f7c6d7304e4a5575c78ebdc62aef4453222d1d0
Felix Koehler <f.koehler@tum.de>
2024-02-08 09:49:44 +0100

    Make PW linear a subclass

--- pdequinox/pointwise_linear_conv.py
+++ pdequinox/pointwise_linear_conv.py
@@ -2,34 +2,32 @@ import jax.numpy as jnp
 import equinox as eqx
 from jaxtyping import Array, Float
 
-class PointwiseLinearConv(eqx.Module):
+class PointwiseLinearConv(eqx.nn.Conv):
     """
     aka 1x1 Convolution; used primarily for channel adjustment
     """
-    
-    conv: eqx.nn.Conv
 
     def __init__(
         self,
         num_spatial_dims: int,
         in_channels: int,
         out_channels: int,
+        use_bias: bool = True,
         *,
         key,
-        use_bias: bool = True,
         zero_bias_init: bool = False,
     ):
-        self.conv = eqx.nn.Conv(
-            num_spatial_dims,
-            in_channels,
-            out_channels,
-            1,
-            key=key,
+        super().__init__(
+            num_spatial_dims=num_spatial_dims,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            dilation=1,
+            padding=0,
             use_bias=use_bias,
+            key=key,
         )
         if use_bias and zero_bias_init:
             zero_bias = jnp.zeros_like(self.conv.bias)
             self.conv = eqx.tree_at(lambda l: l.bias, self.conv, zero_bias)
-
-    def __call__(self, x: Float[Array, "C ..."]) -> Float[Array, "C ..."]:
-        return self.conv(x)
