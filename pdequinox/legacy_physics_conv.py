4810736c9690e5a1b8782ac75089fce64554c4b4
Felix Koehler <f.koehler@tum.de>
2024-02-08 09:43:25 +0100

    Make new physics conv the default

---
+++ pdequinox/legacy_physics_conv.py
@@ -0,0 +1,221 @@
+import jax.numpy as jnp
+import equinox as eqx
+from jaxtyping import Array, Float
+from typing import Tuple, Sequence
+from equinox import field
+
+class BasePadding(eqx.Module):
+    pass
+
+class PeriodicPadding(BasePadding):
+    pad_width: tuple[tuple[int, int], ...] = field(static=True)
+
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        padding: tuple[tuple[int, int], ...],
+    ):
+        self.pad_width = ((0, 0),) + padding
+
+    def __call__(self, x: Float[Array, "C *N"]):
+        """Periodic padding for convolution.
+
+        Args:
+            x: Input array with shape (C, *N).
+
+        Returns:
+            Array with shape (C, *(N + 2 * padding)).
+        """
+        return jnp.pad(x, self.pad_width, mode="wrap")
+
+def compute_valid_padding(
+    kernel_size: tuple[int, ...],
+    dilation: tuple[int, ...],
+) -> tuple[tuple[int, int], ...]:
+    padding_width = tuple(
+        (((k - 1) // 2) * d, ((k - 1) // 2) * d)
+        for k, d in zip(kernel_size, dilation)
+    )
+    return padding_width
+
+def compute_valid_transpose_padding(
+    kernel_size: tuple[int, ...],
+    dilation: tuple[int, ...],
+    padding: tuple[tuple[int, int], ...],
+    output_padding: tuple[int, ...]
+) -> tuple[tuple[int, int], ...]:
+    padding_width = tuple(
+        (d * (k - 1) - p_0, d * (k - 1) - p_1 + o)
+        for k, d, (p_0, p_1), o in zip(kernel_size, dilation, padding, output_padding)
+    )
+    return padding_width
+
+
+class PhysicsConv(eqx.Module):
+    pad: BasePadding
+    conv: eqx.nn.Conv
+    boundary_mode: str
+    boundary_kwargs: dict
+
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        dilation: int = 1,
+        *,
+        boundary_mode: str,
+        use_bias: bool = True,
+        zero_bias_init: bool = False,
+        key,
+        **boundary_kwargs,
+    ):
+        """
+        Only way to reduce the spatial dimensions is to use stride. Otherwise,
+        the convolutions will always be 'valid'.
+        """
+        if kernel_size % 2 == 0:
+            raise ValueError("kernel_size must be odd")
+
+        self.conv = eqx.nn.Conv(
+            num_spatial_dims=num_spatial_dims,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            dilation=dilation,
+            padding=0,  # padding is handled by the pad module
+            groups=1,  # no groups for now
+            key=key,
+            use_bias=use_bias,
+        )
+        if use_bias and zero_bias_init:
+            zero_bias = jnp.zeros_like(self.conv.bias)
+            self.conv = eqx.tree_at(lambda l: l.bias, self.conv, zero_bias)
+
+        self.boundary_mode = boundary_mode.lower()
+        self.boundary_kwargs = boundary_kwargs
+        self._setup_padder()
+
+    def _setup_padder(self):
+        padding_width = compute_valid_padding(
+            self.conv.kernel_size,
+            self.conv.dilation,
+        )
+
+        if self.boundary_mode == "periodic":
+            self.pad = PeriodicPadding(
+                self.conv.num_spatial_dims,
+                padding_width,
+                **self.boundary_kwargs,
+            )
+        else:
+            raise ValueError(f"boundary_mode={self.boundary_mode} not implemented")
+
+
+    def __call__(self, x: Float[Array, "C *N"]) -> Float[Array, "C *N"]:
+        """Periodic convolution.
+
+        Args:
+            x: Input array with shape (C, *N).
+
+        Returns:
+            Array with shape (C, *N).
+        """
+        return self.conv(self.pad(x))
+        
+class PhysicsConvTranspose(eqx.Module):
+    pad: BasePadding
+    conv: eqx.nn.ConvTranspose
+    boundary_mode: str
+    boundary_kwargs: dict
+
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        output_padding: int = 0,
+        dilation: int = 1,
+        *,
+        boundary_mode: str,
+        use_bias: bool = True,
+        zero_bias_init: bool = False,
+        key,
+        **boundary_kwargs,
+    ):
+        if kernel_size % 2 == 0:
+            raise ValueError("kernel_size must be odd")
+
+        self.conv = eqx.nn.ConvTranspose(
+            num_spatial_dims=num_spatial_dims,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            output_padding=0, # padding is handled by the pad module
+            dilation=dilation,
+            padding=0,  # padding is handled by the pad module
+            groups=1,  # no groups for now
+            key=key,
+            use_bias=use_bias,
+        )
+        if use_bias and zero_bias_init:
+            zero_bias = jnp.zeros_like(self.conv.bias)
+            self.conv = eqx.tree_at(lambda l: l.bias, self.conv, zero_bias)
+
+        self.boundary_mode = boundary_mode.lower()
+        self.boundary_kwargs = boundary_kwargs
+        self.pad = self._setup_padder(output_padding)
+    
+    def _setup_output_padding(self, output_padding: int):
+        if isinstance(output_padding, int):
+            output_padding = (output_padding,) * self.conv.num_spatial_dims
+        else:
+            raise ValueError("output_padding must be an int (current limitation)")
+        return output_padding
+    
+    def _setup_padder(self, output_padding: int):
+        valid_padding_width = compute_valid_padding(
+            self.conv.kernel_size,
+            self.conv.dilation,
+        )
+        transpose_padding_width = compute_valid_transpose_padding(
+            self.conv.kernel_size,
+            self.conv.dilation,
+            valid_padding_width,
+            self._setup_output_padding(output_padding),
+        )
+        if self.boundary_mode == "periodic":
+            padder = PeriodicPadding(
+                self.conv.num_spatial_dims,
+                transpose_padding_width,
+                **self.boundary_kwargs,
+            )
+        else:
+            raise ValueError(f"boundary_mode={self.boundary_mode} not implemented")
+        
+        return padder
+
+
+    def __call__(
+        self,
+        x: Float[Array, "C *N"],
+        *,
+        output_padding: int or None = None,
+    ) -> Float[Array, "C *N"]:
+        """
+        Use `output_padding` to overwrite the output padding specified in the
+        constructor.
+        """
+        if output_padding is not None:
+            padder = self._setup_padder(output_padding)
+        else:
+            padder = self.pad
+
+        return self.conv(padder(x))
+
--- pdequinox/physics_conv.py
+++ pdequinox/physics_conv.py
@@ -1,221 +1,352 @@
+"""
+Instead of monkey patching the equinox conv operations (and not succeeding with
+periodic/circular padding on transpose convolutions), I will just implement them
+myselves, but following equinox as close as possible
+"""
+
+import itertools as it
+import math
+from collections.abc import Callable, Sequence
+from typing import Optional, TypeVar, Union, Any
+
+import jax
+import jax.lax as lax
 import jax.numpy as jnp
-import equinox as eqx
-from jaxtyping import Array, Float
-from typing import Tuple, Sequence
-from equinox import field
-
-class BasePadding(eqx.Module):
-    pass
-
-class PeriodicPadding(BasePadding):
-    pad_width: tuple[tuple[int, int], ...] = field(static=True)
-
-    def __init__(
-        self,
-        num_spatial_dims: int,
-        padding: tuple[tuple[int, int], ...],
-    ):
-        self.pad_width = ((0, 0),) + padding
-
-    def __call__(self, x: Float[Array, "C *N"]):
-        """Periodic padding for convolution.
-
-        Args:
-            x: Input array with shape (C, *N).
-
-        Returns:
-            Array with shape (C, *(N + 2 * padding)).
-        """
-        return jnp.pad(x, self.pad_width, mode="wrap")
-
-def compute_valid_padding(
-    kernel_size: tuple[int, ...],
-    dilation: tuple[int, ...],
-) -> tuple[tuple[int, int], ...]:
-    padding_width = tuple(
-        (((k - 1) // 2) * d, ((k - 1) // 2) * d)
-        for k, d in zip(kernel_size, dilation)
-    )
-    return padding_width
-
-def compute_valid_transpose_padding(
-    kernel_size: tuple[int, ...],
-    dilation: tuple[int, ...],
-    padding: tuple[tuple[int, int], ...],
-    output_padding: tuple[int, ...]
-) -> tuple[tuple[int, int], ...]:
-    padding_width = tuple(
-        (d * (k - 1) - p_0, d * (k - 1) - p_1 + o)
-        for k, d, (p_0, p_1), o in zip(kernel_size, dilation, padding, output_padding)
-    )
-    return padding_width
+import jax.random as jrandom
+import numpy as np
+from equinox import Module, field
+from jaxtyping import Array, PRNGKeyArray
 
 
-class PhysicsConv(eqx.Module):
-    pad: BasePadding
-    conv: eqx.nn.Conv
-    boundary_mode: str
-    boundary_kwargs: dict
 
-    def __init__(
-        self,
-        num_spatial_dims: int,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int,
-        stride: int = 1,
-        dilation: int = 1,
-        *,
-        boundary_mode: str,
-        use_bias: bool = True,
-        zero_bias_init: bool = False,
-        key,
-        **boundary_kwargs,
-    ):
-        """
-        Only way to reduce the spatial dimensions is to use stride. Otherwise,
-        the convolutions will always be 'valid'.
-        """
-        if kernel_size % 2 == 0:
-            raise ValueError("kernel_size must be odd")
+_T = TypeVar("_T")
 
-        self.conv = eqx.nn.Conv(
-            num_spatial_dims=num_spatial_dims,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            dilation=dilation,
-            padding=0,  # padding is handled by the pad module
-            groups=1,  # no groups for now
-            key=key,
-            use_bias=use_bias,
-        )
-        if use_bias and zero_bias_init:
-            zero_bias = jnp.zeros_like(self.conv.bias)
-            self.conv = eqx.tree_at(lambda l: l.bias, self.conv, zero_bias)
 
-        self.boundary_mode = boundary_mode.lower()
-        self.boundary_kwargs = boundary_kwargs
-        self._setup_padder()
-
-    def _setup_padder(self):
-        padding_width = compute_valid_padding(
-            self.conv.kernel_size,
-            self.conv.dilation,
-        )
-
-        if self.boundary_mode == "periodic":
-            self.pad = PeriodicPadding(
-                self.conv.num_spatial_dims,
-                padding_width,
-                **self.boundary_kwargs,
-            )
+def _ntuple(n: int) -> Callable[[Union[_T, Sequence[_T]]], tuple[_T, ...]]:
+    def parse(x: Union[_T, Sequence[_T]]) -> tuple[_T, ...]:
+        if isinstance(x, Sequence):
+            if len(x) == n:
+                return tuple(x)
+            else:
+                raise ValueError(
+                    f"Length of {x} (length = {len(x)}) is not equal to {n}"
+                )
         else:
-            raise ValueError(f"boundary_mode={self.boundary_mode} not implemented")
+            return tuple(it.repeat(x, n))
+
+    return parse
+
+def all_sequences(x: Union[Sequence[Any], Sequence[_T]]) -> bool:
+    return all(isinstance(xi, Sequence) for xi in x)
+
+def periodic_padding(
+    x: Array, padding: tuple[tuple[int, int], ...]
+) -> Array:
+    """Periodic padding for convolution.
+
+    Args:
+        x: Input array with shape (C, *N).
+        padding: Padding to apply before and after each spatial dimension.
+
+    Returns:
+        Array with shape (C, *(N + 2 * padding)).
+    """
+    return np.pad(x, ((0, 0),) + padding, mode="wrap")
 
 
-    def __call__(self, x: Float[Array, "C *N"]) -> Float[Array, "C *N"]:
-        """Periodic convolution.
+class PhysicsConv(Module, strict=True):
+    """General N-dimensional convolution."""
 
-        Args:
-            x: Input array with shape (C, *N).
-
-        Returns:
-            Array with shape (C, *N).
-        """
-        return self.conv(self.pad(x))
-        
-class PhysicsConvTranspose(eqx.Module):
-    pad: BasePadding
-    conv: eqx.nn.ConvTranspose
-    boundary_mode: str
-    boundary_kwargs: dict
+    num_spatial_dims: int = field(static=True)
+    weight: Array
+    bias: Optional[Array]
+    in_channels: int = field(static=True)
+    out_channels: int = field(static=True)
+    kernel_size: tuple[int, ...] = field(static=True)
+    stride: tuple[int, ...] = field(static=True)
+    padding: tuple[tuple[int, int], ...] = field(static=True)
+    dilation: tuple[int, ...] = field(static=True)
+    groups: int = field(static=True)
+    use_bias: bool = field(static=True)
+    boundary_mode: str = field(static=True)
+    boundary_kwargs: dict = field(static=True)
 
     def __init__(
         self,
         num_spatial_dims: int,
         in_channels: int,
         out_channels: int,
-        kernel_size: int,
-        stride: int = 1,
-        output_padding: int = 0,
-        dilation: int = 1,
-        *,
-        boundary_mode: str,
+        kernel_size: Union[int, Sequence[int]],
+        stride: Union[int, Sequence[int]] = 1,
+        # no padding because it always chosen to retain spatial size
+        dilation: Union[int, Sequence[int]] = 1,
+        groups: int = 1,
         use_bias: bool = True,
-        zero_bias_init: bool = False,
-        key,
+        *,
+        key: PRNGKeyArray,
+        boundary_mode: str,
         **boundary_kwargs,
     ):
-        if kernel_size % 2 == 0:
-            raise ValueError("kernel_size must be odd")
+        wkey, bkey = jrandom.split(key, 2)
 
-        self.conv = eqx.nn.ConvTranspose(
-            num_spatial_dims=num_spatial_dims,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            output_padding=0, # padding is handled by the pad module
-            dilation=dilation,
-            padding=0,  # padding is handled by the pad module
-            groups=1,  # no groups for now
-            key=key,
-            use_bias=use_bias,
-        )
-        if use_bias and zero_bias_init:
-            zero_bias = jnp.zeros_like(self.conv.bias)
-            self.conv = eqx.tree_at(lambda l: l.bias, self.conv, zero_bias)
+        parse = _ntuple(num_spatial_dims)
+        kernel_size = parse(kernel_size)
+        stride = parse(stride)
+        dilation = parse(dilation)
 
-        self.boundary_mode = boundary_mode.lower()
-        self.boundary_kwargs = boundary_kwargs
-        self.pad = self._setup_padder(output_padding)
-    
-    def _setup_output_padding(self, output_padding: int):
-        if isinstance(output_padding, int):
-            output_padding = (output_padding,) * self.conv.num_spatial_dims
-        else:
-            raise ValueError("output_padding must be an int (current limitation)")
-        return output_padding
-    
-    def _setup_padder(self, output_padding: int):
-        valid_padding_width = compute_valid_padding(
-            self.conv.kernel_size,
-            self.conv.dilation,
+        if in_channels % groups != 0:
+            raise ValueError(
+                f"`in_channels` (={in_channels}) must be divisible "
+                f"by `groups` (={groups})."
+            )
+
+        grouped_in_channels = in_channels // groups
+        lim = 1 / np.sqrt(grouped_in_channels * math.prod(kernel_size))
+        self.weight = jrandom.uniform(
+            wkey,
+            (out_channels, grouped_in_channels) + kernel_size,
+            minval=-lim,
+            maxval=lim,
         )
-        transpose_padding_width = compute_valid_transpose_padding(
-            self.conv.kernel_size,
-            self.conv.dilation,
-            valid_padding_width,
-            self._setup_output_padding(output_padding),
-        )
-        if self.boundary_mode == "periodic":
-            padder = PeriodicPadding(
-                self.conv.num_spatial_dims,
-                transpose_padding_width,
-                **self.boundary_kwargs,
+        if use_bias:
+            self.bias = jrandom.uniform(
+                bkey,
+                (out_channels,) + (1,) * num_spatial_dims,
+                minval=-lim,
+                maxval=lim,
             )
         else:
+            self.bias = None
+
+        self.num_spatial_dims = num_spatial_dims
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+        padding = tuple(
+            ((k - 1) * d // 2, (k - 1) * d // 2)
+            for k, d in zip(kernel_size, dilation)
+        )
+
+        if isinstance(padding, int):
+            self.padding = tuple((padding, padding) for _ in range(num_spatial_dims))
+        elif isinstance(padding, Sequence) and len(padding) == num_spatial_dims:
+            if all_sequences(padding):
+                self.padding = tuple(padding)
+            else:
+                self.padding = tuple((p, p) for p in padding)
+        else:
+            raise ValueError(
+                "`padding` must either be an int or tuple of length "
+                f"{num_spatial_dims} containing ints or tuples of length 2."
+            )
+        self.dilation = dilation
+        self.groups = groups
+        self.use_bias = use_bias
+        self.boundary_mode = boundary_mode
+        self.boundary_kwargs = boundary_kwargs
+
+    @jax.named_scope("PhysicsConv")
+    def __call__(self, x: Array, *, key: Optional[PRNGKeyArray] = None) -> Array:
+        """**Arguments:**
+
+        - `x`: The input. Should be a JAX array of shape
+            `(in_channels, dim_1, ..., dim_N)`, where `N = num_spatial_dims`.
+        - `key`: Ignored; provided for compatibility with the rest of the Equinox API.
+            (Keyword only argument.)
+
+        **Returns:**
+
+        A JAX array of shape `(out_channels, new_dim_1, ..., new_dim_N)`.
+        """
+
+        unbatched_rank = self.num_spatial_dims + 1
+        if x.ndim != unbatched_rank:
+            raise ValueError(
+                f"Input to `Conv` needs to have rank {unbatched_rank},",
+                f" but input has shape {x.shape}.",
+            )
+        if self.boundary_mode == "periodic":
+            x = periodic_padding(x, self.padding)
+        else:
             raise ValueError(f"boundary_mode={self.boundary_mode} not implemented")
-        
-        return padder
 
+        x = jnp.expand_dims(x, axis=0)
+        padding_lax = ((0, 0),) * self.num_spatial_dims
+        x = lax.conv_general_dilated(
+            lhs=x,
+            rhs=self.weight,
+            window_strides=self.stride,
+            padding=padding_lax,  # no padding is applied here
+            rhs_dilation=self.dilation,
+            feature_group_count=self.groups,
+        )
+        x = jnp.squeeze(x, axis=0)
+        if self.use_bias:
+            x = x + self.bias
+        return x
 
+class PhysicsConvTranspose(Module, strict=True):
+    """General N-dimensional transposed convolution."""
+
+    num_spatial_dims: int = field(static=True)
+    weight: Array
+    bias: Optional[Array]
+    in_channels: int = field(static=True)
+    out_channels: int = field(static=True)
+    kernel_size: tuple[int, ...] = field(static=True)
+    stride: tuple[int, ...] = field(static=True)
+    padding: tuple[tuple[int, int], ...] = field(static=True)
+    output_padding: tuple[int, ...] = field(static=True)
+    dilation: tuple[int, ...] = field(static=True)
+    groups: int = field(static=True)
+    use_bias: bool = field(static=True)
+    boundary_mode: str = field(static=True)
+    boundary_kwargs: dict = field(static=True)
+
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Sequence[int]],
+        stride: Union[int, Sequence[int]] = 1,
+        # no padding because it always chosen to retain spatial size
+        output_padding: Union[int, Sequence[int]] = 0,
+        dilation: Union[int, Sequence[int]] = 1,
+        groups: int = 1,
+        use_bias: bool = True,
+        *,
+        key: PRNGKeyArray,
+        boundary_mode: str,
+        **boundary_kwargs,
+    ):
+        wkey, bkey = jrandom.split(key, 2)
+
+        parse = _ntuple(num_spatial_dims)
+        kernel_size = parse(kernel_size)
+        stride = parse(stride)
+        output_padding = parse(output_padding)
+        dilation = parse(dilation)
+
+        for s, o in zip(stride, output_padding):
+            if output_padding >= stride:
+                raise ValueError("Must have `output_padding < stride` (elementwise).")
+
+        grouped_in_channels = in_channels // groups
+        lim = 1 / np.sqrt(grouped_in_channels * math.prod(kernel_size))
+        self.weight = jrandom.uniform(
+            wkey,
+            (out_channels, grouped_in_channels) + kernel_size,
+            minval=-lim,
+            maxval=lim,
+        )
+        if use_bias:
+            self.bias = jrandom.uniform(
+                bkey,
+                (out_channels,) + (1,) * num_spatial_dims,
+                minval=-lim,
+                maxval=lim,
+            )
+        else:
+            self.bias = None
+
+        self.num_spatial_dims = num_spatial_dims
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+        padding = tuple(
+            (
+                int(np.floor((k - 1) * d / 2.0)),
+                int(np.ceil ((k - 1) * d / 2.0)),
+            )
+            for k, d in zip(kernel_size, dilation)
+        )
+
+        if isinstance(padding, int):
+            self.padding = tuple((padding, padding) for _ in range(num_spatial_dims))
+        elif isinstance(padding, Sequence) and len(padding) == num_spatial_dims:
+            if all_sequences(padding):
+                self.padding = tuple(padding)
+            else:
+                self.padding = tuple((p, p) for p in padding)
+        else:
+            raise ValueError(
+                "`padding` must either be an int or tuple of length "
+                f"{num_spatial_dims} containing ints or tuples of length 2."
+            )
+        self.output_padding = output_padding
+        self.dilation = dilation
+        self.groups = groups
+        self.use_bias = use_bias
+        self.boundary_mode = boundary_mode
+        self.boundary_kwargs = boundary_kwargs
+
+    @jax.named_scope("PhysicsConvTranspose")
     def __call__(
         self,
-        x: Float[Array, "C *N"],
+        x: Array,
         *,
-        output_padding: int or None = None,
-    ) -> Float[Array, "C *N"]:
-        """
-        Use `output_padding` to overwrite the output padding specified in the
-        constructor.
+        key: Optional[PRNGKeyArray] = None,
+        output_padding: Optional[Union[int, Sequence[int]]] = None,
+    ) -> Array:
+        """**Arguments:**
+
+        - `x`: The input. Should be a JAX array of shape
+            `(in_channels, dim_1, ..., dim_N)`, where `N = num_spatial_dims`.
+        - `key`: Ignored; provided for compatibility with the rest of the Equinox API.
+            (Keyword only argument.)
+        - `output_padding`: Overwrite the `output_padding` attribute specified in the
+            constructor for this call only. If `None`, the attribute is used. (Keyword
+            only argument.)
+
+        **Returns:**
+
+        A JAX array of shape `(out_channels, new_dim_1, ..., new_dim_N)`.
         """
         if output_padding is not None:
-            padder = self._setup_padder(output_padding)
+            parse = _ntuple(self.num_spatial_dims)
+            this_output_padding = parse(output_padding)
         else:
-            padder = self.pad
+            this_output_padding = self.output_padding
 
-        return self.conv(padder(x))
+        unbatched_rank = self.num_spatial_dims + 1
+        if x.ndim != unbatched_rank:
+            raise ValueError(
+                f"Input to `ConvTranspose` needs to have rank {unbatched_rank},",
+                f" but input has shape {x.shape}.",
+            )
+        padding = tuple(
+            (
+                int(np.floor((d * (k - 1) - p0) / s)),
+                int( np.ceil((d * (k - 1) - p1) / s)),
+            )
+            for k, s, (p0, p1), o, d in zip(
+                self.kernel_size, self.stride, self.padding, this_output_padding, self.dilation
+            )
+        )
+        if self.boundary_mode == "periodic":
+            x = periodic_padding(x, padding)
+        else:
+            raise ValueError(f"boundary_mode={self.boundary_mode} not implemented")
 
+        x = jnp.expand_dims(x, axis=0)
+        # ToDo: is this correct?
+        padding_lax = ((0, o) for o in this_output_padding)
+        # padding_lax = ((0, 0),) * self.num_spatial_dims
+        x = lax.conv_general_dilated(
+            lhs=x,
+            rhs=self.weight,
+            window_strides=(1,) * self.num_spatial_dims,
+            padding=padding_lax,  # no padding is applied here, only output padding to correct shapes
+            lhs_dilation=self.stride,
+            rhs_dilation=self.dilation,
+            feature_group_count=self.groups,
+        )
+        x = jnp.squeeze(x, axis=0)
+        if self.use_bias:
+            x = x + self.bias
+        return x
