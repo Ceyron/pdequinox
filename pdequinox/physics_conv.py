49d89286e9586d5fa0758fe4f33938c3c899faa9
Felix Koehler <f.koehler@tum.de>
2024-02-14 14:49:32 +0100

    Fix and simplify the transpose conv

--- pdequinox/physics_conv.py
+++ pdequinox/physics_conv.py
@@ -180,7 +180,16 @@ class PhysicsConv(Module, strict=True):
         return x
 
 class PhysicsConvTranspose(Module, strict=True):
-    """General N-dimensional transposed convolution."""
+    """General N-dimensional transposed convolution.
+    
+    inspired by circular padding in jax flax:
+    https://github.com/google/flax/blob/c25f546ff4c72c6c85e5225fe8467331720034be/flax/linen/linear.py#L864
+
+    and jax-cfd:
+    https://github.com/google/jax-cfd/blob/d215f13282bd63045fb3455f8fac061653428040/jax_cfd/ml/layers.py#L218
+
+    ToDo: Does not yet support dilation!
+    """
 
     num_spatial_dims: int = field(static=True)
     weight: Array
@@ -215,6 +224,9 @@ class PhysicsConvTranspose(Module, strict=True):
         zero_bias_init: bool = False,
         **boundary_kwargs,
     ):
+        if dilation != 1:
+            raise NotImplementedError("Dilation not yet supported in `ConvTranspose`.")
+
         wkey, bkey = jrandom.split(key, 2)
 
         parse = _ntuple(num_spatial_dims)
@@ -254,26 +266,10 @@ class PhysicsConvTranspose(Module, strict=True):
         self.kernel_size = kernel_size
         self.stride = stride
 
-        padding = tuple(
-            (
-                int(np.floor((k - 1) * d / 2.0)),
-                int(np.ceil ((k - 1) * d / 2.0)),
-            )
-            for k, d in zip(kernel_size, dilation)
-        )
+        # Note that those are effectively multiplied by stride because we use
+        # the lhs dilation
+        self.padding = tuple((k//2, k//2) for k in kernel_size)
 
-        if isinstance(padding, int):
-            self.padding = tuple((padding, padding) for _ in range(num_spatial_dims))
-        elif isinstance(padding, Sequence) and len(padding) == num_spatial_dims:
-            if all_sequences(padding):
-                self.padding = tuple(padding)
-            else:
-                self.padding = tuple((p, p) for p in padding)
-        else:
-            raise ValueError(
-                "`padding` must either be an int or tuple of length "
-                f"{num_spatial_dims} containing ints or tuples of length 2."
-            )
         self.output_padding = output_padding
         self.dilation = dilation
         self.groups = groups
@@ -315,34 +311,40 @@ class PhysicsConvTranspose(Module, strict=True):
                 f"Input to `ConvTranspose` needs to have rank {unbatched_rank},",
                 f" but input has shape {x.shape}.",
             )
-        padding = tuple(
-            (
-                int(np.floor((d * (k - 1) - p0) / s)),
-                int( np.ceil((d * (k - 1) - p1) / s)),
-            )
-            for k, s, (p0, p1), o, d in zip(
-                self.kernel_size, self.stride, self.padding, this_output_padding, self.dilation
-            )
-        )
+        
+        # needed for slicing out later
+        spatial_shape = x.shape[1:]
+        
         if self.boundary_mode == "periodic":
-            x = periodic_padding(x, padding)
+            x = periodic_padding(x, self.padding)
         else:
             raise ValueError(f"boundary_mode={self.boundary_mode} not implemented")
 
         x = jnp.expand_dims(x, axis=0)
-        # ToDo: is this correct?
-        padding_lax = ((0, o) for o in this_output_padding)
-        # padding_lax = ((0, 0),) * self.num_spatial_dims
+        padding_lax = ((0, 0),) * self.num_spatial_dims
         x = lax.conv_general_dilated(
             lhs=x,
             rhs=self.weight,
             window_strides=(1,) * self.num_spatial_dims,
-            padding=padding_lax,  # no padding is applied here, only output padding to correct shapes
+            padding=padding_lax, # no padding is applied here
             lhs_dilation=self.stride,
             rhs_dilation=self.dilation,
             feature_group_count=self.groups,
         )
         x = jnp.squeeze(x, axis=0)
+        
+        output_slices = ((None, None),) + tuple(
+            (
+                (k // s) * (s - 1),
+                (k // s) * (s - 1) + s * dims - 1 + o,
+            )
+            for s, k, dims, o in zip(self.stride, self.kernel_size, spatial_shape, this_output_padding)
+        )
+        output_slices = tuple(slice(*entry) for entry in output_slices)
+
+        # Following JAX-CFD
+        x = x[output_slices]
+
         if self.use_bias:
             x = x + self.bias
         return x
