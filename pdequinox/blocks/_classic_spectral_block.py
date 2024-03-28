67a6bcb0c3375f5b2df02d3df891d443c2ecb21e
Felix Köhler <felix.matteo@t-online.de>
2024-03-28 17:13:58 +0100

    Add docs

--- pdequinox/blocks/_classic_spectral_block.py
+++ pdequinox/blocks/_classic_spectral_block.py
@@ -19,12 +19,38 @@ class ClassicSpectralBlock(Block):
         out_channels: int,
         *,
         key: PRNGKeyArray,
-        num_modes: int = 8,
         # Uses gelu because it likely recovers more modes
         activation: Callable = jax.nn.gelu,
+        num_modes: int = 8,
         use_bias: bool = True,
         zero_bias_init: bool = False,
     ):
+        """
+        Residual-style block as used in vanilla FNOs; combines a spectral
+        convolution with a bypass.
+
+        Does not have argument `boundary_mode` because it would not respect it.
+        In the original FNO paper it is argued that the bypass helps recover the
+        boundary condition.
+
+        **Arguments:**
+
+        - `num_spatial_dims`: The number of spatial dimensions. For example
+            traditional convolutions for image processing have this set to `2`.
+        - `in_channels`: The number of input channels.
+        - `out_channels`: The number of output channels.
+        - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+        - `activation`: The activation function to use after each convolution.
+            Default is `jax.nn.relu`.
+        - `num_modes`: How many modes to consider in Fourier space. At max this
+            can be N//2+1, with N being the number of spatial points. Think of
+            it as the analogy of the kernel size.
+        - `use_bias`: Whether to use a bias in the bypass convolution. Default
+          `True`.
+        - `zero_bias_init`: Whether to initialise the bias to zero. Default is
+            `False`.
+        """
         k_1, k_2 = jax.random.split(key)
         self.spectral_conv = SpectralConv(
             num_spatial_dims=num_spatial_dims,
@@ -55,8 +81,32 @@ class ClassicSpectralBlock(Block):
 
 class ClassicSpectralBlockFactory(BlockFactory):
     num_modes: Union[int, tuple[int, ...]]
-    use_bias: bool = True
-    zero_bias_init: bool = False
+    use_bias: bool
+    zero_bias_init: bool
+
+    def __init__(
+        self,
+        *,
+        num_modes: int = 8,
+        use_bias: bool = True,
+        zero_bias_init: bool = False,
+    ):
+        """
+        Factory for creating `ClassicSpectralBlock` instances.
+
+        **Arguments:**
+
+        - `num_modes`: How many modes to consider in Fourier space. At max this
+            can be N//2+1, with N being the number of spatial points. Think of
+            it as the analogy of the kernel size.
+        - `use_bias`: Whether to use a bias in the bypass convolution. Default
+          `True`.
+        - `zero_bias_init`: Whether to initialise the bias to zero. Default is
+            `False`.
+        """
+        self.num_modes = num_modes
+        self.use_bias = use_bias
+        self.zero_bias_init = zero_bias_init
 
     def __call__(
         self,
