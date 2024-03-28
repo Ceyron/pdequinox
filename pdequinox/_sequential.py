5a43f70f3d6e1831044e7a76f7dc4fed93bdfa03
Felix Köhler <felix.matteo@t-online.de>
2024-03-28 07:08:01 +0100

    Add option to vary the hidden channels between the blocks

--- pdequinox/_sequential.py
+++ pdequinox/_sequential.py
@@ -1,4 +1,4 @@
-from typing import Callable, List
+from typing import Callable, List, Sequence, Union
 
 import equinox as eqx
 import jax.random as jr
@@ -23,7 +23,7 @@ class Sequential(eqx.Module):
         num_spatial_dims: int,
         in_channels: int,
         out_channels: int,
-        hidden_channels: int,
+        hidden_channels: Union[Sequence[int], int],
         num_blocks: int,
         activation: Callable,
         *,
@@ -35,7 +35,7 @@ class Sequential(eqx.Module):
         **boundary_kwargs,
     ):
         """
-        Generic constructor for block-based architectures like ResNets.
+        Generic constructor for sequential block-based architectures like ResNets.
 
         **Arguments:**
 
@@ -43,8 +43,11 @@ class Sequential(eqx.Module):
             traditional convolutions for image processing have this set to `2`.
         - `in_channels`: The number of input channels.
         - `out_channels`: The number of output channels.
-        - `hidden_channels`: The number of channels in the hidden layers.
-        - `num_blocks`: The number of blocks to use. TODO add clarification
+        - `hidden_channels`: The number of channels in the hidden layers. Either
+            an integer to have the same number of hidden channels in the layers
+            between all blocks, or a list of `num_blocks + 1` integers.
+        - `num_blocks`: The number of blocks to use. Must be an integer greater
+            equal than `1`.
         - `activation`: The activation function to use in the blocks.
         - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
             initialisation. (Keyword only argument.)
@@ -63,24 +66,33 @@ class Sequential(eqx.Module):
         subkey, key = jr.split(key)
         if num_blocks < 1:
             raise ValueError("num_blocks must be at least 1")
+        
+        if isinstance(hidden_channels, int):
+            hidden_channels = (hidden_channels,) * (num_blocks + 1)
+        else:
+            if len(hidden_channels) != (num_blocks + 1):
+                raise ValueError("The list of hidden channels must be one longer than the number of blocks")
 
         self.lifting = lifting_factory(
             num_spatial_dims=num_spatial_dims,
             in_channels=in_channels,
-            out_channels=hidden_channels,
+            out_channels=hidden_channels[0],
             activation=activation,
             boundary_mode=boundary_mode,
             key=subkey,
             **boundary_kwargs,
         )
         self.blocks = []
-        for _ in range(num_blocks):
+        for fan_in, fan_out in zip(
+            hidden_channels[:-1],
+            hidden_channels[1:],
+        ):
             subkey, key = jr.split(key)
             self.blocks.append(
                 block_factory(
                     num_spatial_dims=num_spatial_dims,
-                    in_channels=hidden_channels,
-                    out_channels=hidden_channels,
+                    in_channels=fan_in,
+                    out_channels=fan_out,
                     activation=activation,
                     boundary_mode=boundary_mode,
                     key=subkey,
@@ -89,7 +101,7 @@ class Sequential(eqx.Module):
             )
         self.projection = projection_factory(
             num_spatial_dims=num_spatial_dims,
-            in_channels=hidden_channels,
+            in_channels=hidden_channels[-1],
             out_channels=out_channels,
             activation=activation,
             boundary_mode=boundary_mode,
