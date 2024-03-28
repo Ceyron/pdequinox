f9543b4741c0cf1cc17183806fa4908035978601
Felix Köhler <felix.matteo@t-online.de>
2024-03-28 15:38:03 +0100

    Classic Resblock has in/out-channels and allows for group norm

--- pdequinox/blocks/_classic_res_block.py
+++ pdequinox/blocks/_classic_res_block.py
@@ -4,61 +4,90 @@ import equinox as eqx
 import jax
 from jaxtyping import PRNGKeyArray
 
-from ..conv import PhysicsConv
+from ..conv import PhysicsConv, PointwiseLinearConv
 
 
 class ClassicResBlock(eqx.Module):
     conv_1: eqx.Module
+    norm_1: eqx.Module
     conv_2: eqx.Module
+    norm_2: eqx.Module
+    bypass_conv: eqx.Module
+    bypass_norm: eqx.Module
     activation: Callable
 
     def __init__(
         self,
         num_spatial_dims: int,
-        channels: int,
+        in_channels: int,
+        out_channels: int,
         *,
         boundary_mode: str,
         key: PRNGKeyArray,
         activation: Callable = jax.nn.relu,
         kernel_size: int = 3,
+        use_norm: bool = False,
+        num_groups: int = 1,  # for group norm
         use_bias: bool = True,
         zero_bias_init: bool = False,
         **boundary_kwargs,
     ):
-        k_1, k_2 = jax.random.split(key)
-        self.conv_1 = PhysicsConv(
-            num_spatial_dims=num_spatial_dims,
-            in_channels=channels,
-            out_channels=channels,
-            kernel_size=kernel_size,
-            stride=1,
-            dilation=1,
-            boundary_mode=boundary_mode,
-            use_bias=use_bias,
-            zero_bias_init=zero_bias_init,
-            key=k_1,
-            **boundary_kwargs,
-        )
-        self.conv_2 = PhysicsConv(
-            num_spatial_dims=num_spatial_dims,
-            in_channels=channels,
-            out_channels=channels,
-            kernel_size=kernel_size,
-            stride=1,
-            dilation=1,
-            boundary_mode=boundary_mode,
-            use_bias=use_bias,
-            zero_bias_init=zero_bias_init,
-            key=k_2,
-            **boundary_kwargs,
-        )
+        def conv_constructor(i, o, b, k):
+            return PhysicsConv(
+                num_spatial_dims=num_spatial_dims,
+                in_channels=i,
+                out_channels=o,
+                kernel_size=kernel_size,
+                stride=1,
+                dilation=1,
+                boundary_mode=boundary_mode,
+                use_bias=b,
+                zero_bias_init=zero_bias_init,
+                key=k,
+                **boundary_kwargs,
+            )
+
+        k_1, k_2, key = jax.random.split(key, 3)
+        self.conv_1 = conv_constructor(in_channels, out_channels, use_bias, k_1)
+        if use_norm:
+            self.norm_1 = eqx.nn.GroupNorm(groups=num_groups, channels=out_channels)
+        else:
+            self.norm_1 = eqx.nn.Identity
+        
+        self.conv_2 = conv_constructor(out_channels, out_channels, use_bias, k_2)
+        if use_norm:
+            self.norm_2 = eqx.nn.GroupNorm(groups=num_groups, channels=out_channels)
+        else:
+            self.norm_2 = eqx.nn.Identity
+        
+        if out_channels != in_channels:
+            bypass_conv_key, _ = jax.random.split(key)
+            self.bypass_conv = PointwiseLinearConv(
+                num_spatial_dims=num_spatial_dims,
+                in_channels=in_channels,
+                out_channels=out_channels,
+                use_bias=False,  # Following PDEArena
+                key=bypass_conv_key,
+            )
+            if use_norm:
+                self.bypass_norm = eqx.nn.GroupNorm(
+                    groups=num_groups, channels=out_channels
+                )
+            else:
+                self.bypass_norm = eqx.nn.Identity()
+        else:
+            self.bypass_conv = eqx.nn.Identity()
+            self.bypass_norm = eqx.nn.Identity()
+        
         self.activation = activation
 
     def __call__(self, x):
         x_skip = x
         x = self.conv_1(x)
+        x = self.norm_1(x)
         x = self.activation(x)
         x = self.conv_2(x)
+        x = self.norm_2(x)
         x = x + x_skip
         x = self.activation(x)
         return x
@@ -105,15 +134,10 @@ class ClassicResBlockFactory(eqx.Module):
         key: PRNGKeyArray,
         **boundary_kwargs,
     ):
-        if in_channels != out_channels:
-            raise ValueError(
-                "ClassicResBlock only supports in_channels == out_channels"
-            )
-        else:
-            channels = in_channels
         return ClassicResBlock(
             num_spatial_dims,
-            channels,
+            in_channels,
+            out_channels,
             activation=activation,
             kernel_size=self.kernel_size,
             boundary_mode=boundary_mode,
