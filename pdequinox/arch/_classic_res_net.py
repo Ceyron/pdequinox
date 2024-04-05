f3d3d6df8cb58d3128e2fb1e2869fbebd401878b
Felix Koehler <f.koehler@tum.de>
2024-04-05 15:37:32 +0200

    Add doc

--- pdequinox/arch/_classic_res_net.py
+++ pdequinox/arch/_classic_res_net.py
@@ -23,7 +23,35 @@ class ClassicResNet(Sequential):
         **boundary_kwargs,
     ):
         """
-        Classic ResNet
+        Vanilla ResNet architecture very close the original He et al. (2016)
+        paper.
+
+        Performs a sequence of blocks consisting of two convolutions and a
+        bypass. The structure of the blocks are "post-activation" (original
+        ResNet paper). For the modern "pre-activation" ResNet, see
+        `ModernResNet`. By default, no group normalization is used. The original
+        paper used batch normalization.
+
+        The total number of convolutions is `2 * num_blocks` (3x3 convolutions)
+        and two 1x1 convolutions for the lifting and projection.
+
+        **Arguments:**
+
+        - `num_spatial_dims`: The number of spatial dimensions. For example
+            traditional convolutions for image processing have this set to `2`.
+        - `in_channels`: The number of input channels.
+        - `out_channels`: The number of output channels.
+        - `hidden_channels`: The number of channels in the hidden layers.
+            Default is `32`.
+        - `num_blocks`: The number of blocks to use. Must be an integer greater
+            or equal than `1`. Default is `6`.
+        - `use_norm`: Whether to use group normalization. Default is `False`.
+        - `activation`: The activation function to use in the blocks. Default is
+            `jax.nn.relu`. Lifting and projection are **not** activated.
+        - `boundary_mode`: The boundary mode to use for the convolution. Default
+            is `"periodic"`.
+        - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
         """
         super().__init__(
             num_spatial_dims=num_spatial_dims,
