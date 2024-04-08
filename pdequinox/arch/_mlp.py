c9c60edacde26d971b42acdd7ed2c8ed193dbdf8
Felix Koehler <f.koehler@tum.de>
2024-04-08 14:37:20 +0200

    Add docs

--- pdequinox/arch/_mlp.py
+++ pdequinox/arch/_mlp.py
@@ -35,6 +35,38 @@ class MLP(eqx.Module):
         use_final_bias: bool = True,
         key: PRNGKeyArray,
     ):
+        """
+        A MLP for the conv format.
+
+        Takes states of shape `(in_channels, #num_points)` with a leading
+        `in_channels` axis and as many spatial axes as `num_spatial_dims`.
+        Internally, the input is flattened and given to a classical MLP. The
+        conv shape is restored in the end.
+
+        Contrary to convolutional networks, the `num_points` must be supplied!
+
+        **Arguments:**
+
+        - `num_spatial_dims`: The number of spatial dimensions. For example
+            traditional convolutions for image processing have this set to `2`.
+        - `in_channels`: The number of input channels.
+        - `out_channels`: The number of output channels.
+        - `num_points`: The number of points in each spatial dimension. Must be
+            supplied.
+        - `width_size`: The width of the hidden layers. Default is `64`.
+        - `depth`: The number of hidden layers. Default is `3`. The number of
+            linear-affine transformations performed is `depth + 1`.
+        - `activation`: The activation function to use in the hidden layers.
+            Default is `jax.nn.relu`.
+        - `final_activation`: The activation function to use in the final layer.
+            Default is the identity function.
+        - `use_bias`: If `True`, uses bias in the hidden layers. Default is
+            `True`.
+        - `use_final_bias`: If `True`, uses bias in the final layer. Default is
+            `True`.
+        - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+        """
         self.num_spatial_dims = num_spatial_dims
         self.num_points = num_points
         self.in_channels = in_channels
