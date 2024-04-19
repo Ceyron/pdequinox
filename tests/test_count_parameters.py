2082b48b30915152893a1c83b3ddeeac5f749cc5
Felix Koehler <f.koehler@tum.de>
2024-04-19 14:47:41 +0200

    Parameter tests for first block

--- tests/test_count_parameters.py
+++ tests/test_count_parameters.py
@@ -2,12 +2,14 @@
 Validates the correctness of the implementations by comparing the number of
 parameters to the expected number of parameters.
 """
-from typing import Literal
+from typing import Callable, Literal
 
 import jax
 import pytest
 
 import pdequinox as pdeqx
+
+# Basic Convolutional Layers
 
 
 @pytest.mark.parametrize(
@@ -141,3 +143,96 @@ def test_pointwise_linear_conv(
         num_parameters_expected += out_channels
 
     assert num_parameters == num_parameters_expected
+
+
+# Blocks
+
+
+@pytest.mark.parametrize(
+    "num_spatial_dims,in_channels,out_channels,boundary_mode,activation,kernel_size,use_norm,use_bias,zero_bias_init",
+    [
+        (
+            num_spatial_dims,
+            in_channels,
+            out_channels,
+            boundary_mode,
+            activation,
+            kernel_size,
+            use_norm,
+            use_bias,
+            zero_bias_init,
+        )
+        for num_spatial_dims in [1, 2, 3]
+        for in_channels in [1, 2, 5]
+        for out_channels in [1, 2, 5]
+        for boundary_mode in ["periodic", "dirichlet", "neumann"]
+        for activation in [jax.nn.relu, jax.nn.sigmoid]
+        for kernel_size in [2, 3, 4, 5]
+        for use_norm in [True, False]
+        for use_bias in [True, False]
+        for zero_bias_init in [True, False]
+    ],
+)
+def test_ClassicDoubleConvBlock(
+    num_spatial_dims: int,
+    in_channels: int,
+    out_channels: int,
+    boundary_mode: Literal["periodic", "dirichlet", "neumann"],
+    activation: Callable,
+    kernel_size: int,
+    use_norm: bool,
+    use_bias: bool,
+    zero_bias_init: bool,
+):
+    block = pdeqx.blocks.ClassicDoubleConvBlock(
+        num_spatial_dims=num_spatial_dims,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        boundary_mode=boundary_mode,
+        activation=activation,
+        key=jax.random.PRNGKey(0),
+        kernel_size=kernel_size,
+        use_norm=use_norm,
+        use_bias=use_bias,
+        zero_bias_init=zero_bias_init,
+    )
+
+    num_parameters = pdeqx.count_parameters(block)
+
+    # Compute the expected number of parameters
+    first_conv_num_parameters = (
+        in_channels * out_channels * kernel_size**num_spatial_dims
+    ) + (out_channels if use_bias else 0)
+    second_conv_num_parameters = (
+        out_channels * out_channels * kernel_size**num_spatial_dims
+    ) + (out_channels if use_bias else 0)
+    first_norm_num_parameters = 2 * out_channels if use_norm else 0
+    second_norm_num_parameters = 2 * out_channels if use_norm else 0
+
+    num_parameters_expected = (
+        first_conv_num_parameters
+        + second_conv_num_parameters
+        + first_norm_num_parameters
+        + second_norm_num_parameters
+    )
+
+    assert num_parameters == num_parameters_expected
+
+    block_factory = pdeqx.blocks.ClassicDoubleConvBlockFactory(
+        kernel_size=kernel_size,
+        use_norm=use_norm,
+        use_bias=use_bias,
+        zero_bias_init=zero_bias_init,
+    )
+    block_from_factory = block_factory(
+        num_spatial_dims=num_spatial_dims,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        activation=activation,
+        boundary_mode=boundary_mode,
+        key=jax.random.PRNGKey(0),
+    )
+
+    num_parameters_from_factory = pdeqx.count_parameters(block_from_factory)
+
+    assert num_parameters == num_parameters_from_factory
