c1f728670dd81edb970295b639737edc02930368
Felix Koehler <f.koehler@tum.de>
2024-04-22 10:19:21 +0200

    Add custom constructor

--- pdequinox/blocks/_linear_conv_up_block.py
+++ pdequinox/blocks/_linear_conv_up_block.py
@@ -5,7 +5,37 @@ from jaxtyping import PRNGKeyArray
 from ..conv import PhysicsConvTranspose
 from ._base_block import BlockFactory
 
-LinearConvUpBlock = PhysicsConvTranspose
+# LinearConvUpBlock = PhysicsConvTranspose
+
+
+class LinearConvUpBlock(PhysicsConvTranspose):
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        *,
+        kernel_size: int = 3,
+        factor: int = 2,
+        output_padding: int = 1,
+        use_bias: bool = True,
+        zero_bias_init: bool = False,
+        boundary_mode: Literal["periodic", "dirichlet", "neumann"],
+        key: PRNGKeyArray,
+    ):
+        super().__init__(
+            num_spatial_dims=num_spatial_dims,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=factor,
+            output_padding=output_padding,
+            dilation=1,
+            boundary_mode=boundary_mode,
+            use_bias=use_bias,
+            zero_bias_init=zero_bias_init,
+            key=key,
+        )
 
 
 class LinearConvUpBlockFactory(BlockFactory):
@@ -59,7 +89,7 @@ class LinearConvUpBlockFactory(BlockFactory):
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=self.kernel_size,
-            stride=self.factor,
+            factor=self.factor,
             output_padding=self.output_padding,
             boundary_mode=boundary_mode,
             use_bias=self.use_bias,
