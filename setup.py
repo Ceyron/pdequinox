2ddd20d2bfc368fdb939d23deacccfd1cda8b97a
Felix Koehler <f.koehler@tum.de>
2024-10-16 08:09:38 +0200

    No longer need setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="pdequinox",
-    version="0.0.1",
-    description="JAX/Equinox-based architectures for neural operators (=predictors or correctors)",
-    author="Felix Koehler",
-)
